9cc027790a373e958a5f6917bbf4de0a9594288e
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2025-05-02 11:27:46 +0200

    fix: completely redesigned approach to IPFS compatibility and font loading

---
+++ public/fix.js
@@ -0,0 +1,131 @@
+// Early-loading script to fix issues with IPFS deployments
+(function() {
+  // Check for special parameter that indicates we came from the font fixer
+  var forceFix = window.location.search.indexOf('font_fix=true') !== -1;
+  
+  // First, let's try to stop any problematic scripts
+  function stopProblematicScripts() {
+    // Find and stop the existing ipfs-fix.js script
+    var scripts = document.getElementsByTagName('script');
+    for (var i = 0; i < scripts.length; i++) {
+      var script = scripts[i];
+      if (script.src && (
+          script.src.indexOf('ipfs-fix.js') !== -1 || 
+          script.src.indexOf('_ipfs/ipfs-fix.js') !== -1)) {
+        if (script.parentNode) {
+          script.parentNode.removeChild(script);
+          console.log('Removed problematic script:', script.src);
+        }
+      }
+    }
+    
+    // Also stop any inline scripts with regex that might be causing issues
+    var inlineScripts = document.querySelectorAll('script:not([src])');
+    for (var i = 0; i < inlineScripts.length; i++) {
+      var script = inlineScripts[i];
+      var content = script.textContent || '';
+      
+      // Check if it contains both regex and IPFS references
+      if (content.indexOf('match(') !== -1 && 
+          (content.indexOf('ipfs.io') !== -1 || content.indexOf('/_next/') !== -1)) {
+        if (script.parentNode && forceFix) {
+          script.parentNode.removeChild(script);
+          console.log('Removed potentially problematic inline script with regex');
+        }
+      }
+    }
+  }
+  
+  // Run immediately
+  stopProblematicScripts();
+  
+  // Fix all font paths directly
+  function fixFontPaths() {
+    // Create style for fonts
+    var style = document.createElement('style');
+    var fonts = [
+      {name: 'a34f9d1faa5f3315-s.p.woff2', range: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC'},
+      {name: '55c55f0601d81cf3-s.woff2', range: 'U+0460-052F, U+1C80-1C8A, U+20B4'},
+      {name: '26a46d62cd723877-s.woff2', range: 'U+0301, U+0400-045F, U+0490-0491'},
+      {name: '97e0cb1ae144a2a9-s.woff2', range: 'U+1F00-1FFF'},
+      {name: '581909926a08bbc8-s.woff2', range: 'U+0370-0377, U+037A-037F'},
+      {name: 'df0a9ae256c0569c-s.woff2', range: 'U+0102-0103, U+0110-0111'},
+      {name: '6d93bde91c0c2823-s.woff2', range: 'U+0100-02BA, U+02BD-02C5'}
+    ];
+    
+    var css = '';
+    for (var i = 0; i < fonts.length; i++) {
+      var font = fonts[i];
+      css += '@font-face {\n';
+      css += '  font-family: "__Inter_Fallback";\n';
+      css += '  font-style: normal;\n';
+      css += '  font-weight: 100 900;\n';
+      css += '  font-display: swap;\n';
+      css += '  src: url("./fonts/' + font.name + '") format("woff2");\n';
+      css += '  unicode-range: ' + font.range + ';\n';
+      css += '}\n';
+    }
+    
+    style.textContent = css;
+    document.head.appendChild(style);
+    
+    // Create preload links for fonts
+    for (var i = 0; i < fonts.length; i++) {
+      var link = document.createElement('link');
+      link.rel = 'preload';
+      link.href = './fonts/' + fonts[i].name;
+      link.as = 'font';
+      link.type = 'font/woff2';
+      link.crossOrigin = 'anonymous';
+      document.head.appendChild(link);
+    }
+  }
+  
+  // Fix asset paths
+  function fixAssetPaths() {
+    // Fix all existing elements with src or href attributes
+    var elements = document.querySelectorAll('[src], [href]');
+    for (var i = 0; i < elements.length; i++) {
+      var el = elements[i];
+      
+      if (el.hasAttribute('src')) {
+        var src = el.getAttribute('src');
+        if (src && typeof src === 'string') {
+          // Fix paths
+          if (src.indexOf('/_next/') === 0) {
+            el.setAttribute('src', './_next/' + src.substring(7));
+          } else if (src.indexOf('/fonts/') === 0) {
+            el.setAttribute('src', '.' + src);
+          } else if (src.indexOf('/images/') === 0) {
+            el.setAttribute('src', '.' + src);
+          }
+        }
+      }
+      
+      if (el.hasAttribute('href')) {
+        var href = el.getAttribute('href');
+        if (href && typeof href === 'string') {
+          // Fix paths
+          if (href.indexOf('/_next/') === 0) {
+            el.setAttribute('href', './_next/' + href.substring(7));
+          } else if (href.indexOf('/fonts/') === 0) {
+            el.setAttribute('href', '.' + href);
+          } else if (href.indexOf('/images/') === 0) {
+            el.setAttribute('href', '.' + href);
+          }
+        }
+      }
+    }
+  }
+  
+  // Wait for DOM to be loaded before fixing paths
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', function() {
+      fixFontPaths();
+      fixAssetPaths();
+    });
+  } else {
+    fixFontPaths();
+    fixAssetPaths();
+  }
+})(); 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Preload font to ensure it's available */}
+        {/* Preload main font file */}
         <link 
           rel="preload" 
           href="/fonts/a34f9d1faa5f3315-s.p.woff2" 
@@ -14,11 +14,22 @@ export default function Document() {
           crossOrigin="anonymous" 
         />
         
-        {/* IPFS path fixing script - no regex */}
-        <script src="/path-fix.js" />
+        {/* Our early-running fix script - must be loaded first */}
+        <script src="/fix.js" />
         
-        {/* Font specific fixing */}
-        <script src="/font-fix.js" />
+        {/* Add inline styles for font loading for immediate use */}
+        <style dangerouslySetInnerHTML={{
+          __html: `
+            /* Basic font definition that will be enhanced by the script */
+            @font-face {
+              font-family: '__Inter_Fallback';
+              font-style: normal;
+              font-weight: 100 900;
+              font-display: swap;
+              src: url('/fonts/a34f9d1faa5f3315-s.p.woff2') format('woff2');
+            }
+          `
+        }} />
       </Head>
       <body>
         <Main />
