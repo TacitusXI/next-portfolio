e6d4399a4022e2b0d584a0b8b5ad334085538b24
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2025-05-11 16:23:17 +0200

    Update styling and document settings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,16 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WR6XC8MG');`
-        }} />
-        {/* End Google Tag Manager */}
-        
         {/* Early script to check query parameters */}
         <script dangerouslySetInnerHTML={{
           __html: `
@@ -75,12 +65,6 @@ export default function Document() {
         }} />
       </Head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WR6XC8MG"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-        }} />
-        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
